880c5f750c77c27b342288d6ae47a1bd42693e72
Saulo <saulomcm@gmail.com>
2023-09-23 10:28:52 -0300

    Fim da Aula 42

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package br.com.sm.todolist.ionic;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'br.com.sm.todolist.ionic',
   appName: 'ToDoList',
   webDir: 'www',
   server: {
