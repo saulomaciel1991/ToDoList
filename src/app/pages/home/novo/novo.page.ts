144ca707290fb21b408513b355883326b4c18f61
Saulo Maciel <saulomcm@gmail.com>
2023-10-21 10:18:16 -0300

    Implementa a pagina "novo"

--- src/app/pages/home/novo/novo.page.ts
+++ src/app/pages/home/novo/novo.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { TarefaService } from 'src/app/services/tarefa.service';
 
 @Component({
   selector: 'app-novo',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./novo.page.scss'],
 })
 export class NovoPage implements OnInit {
-
-  constructor() { }
+  tarefa! : string 
+  constructor( private tarefaService: TarefaService, private navCrtl : NavController) { }
 
   ngOnInit() {
+  }
+
+  salvar(){
+    let tarefa : any  = {tarefa : this.tarefa}
+    this.tarefaService.salvar(tarefa, this.voltar())
+  }
+
+  voltar(){
+    this.navCrtl.navigateBack('/home')
   }
 
 }
